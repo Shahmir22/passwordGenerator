627cdea8a62dc10012b3b39424f3d62555f3c2d4
ShahmirFarooqui <shahmirfarooqui91@gmail.com>
2020-09-02 23:01:39 -0400

    added fuctionality for password generator

--- script.js
+++ script.js
@@ -14,8 +14,25 @@
 // WHEN the password is generated
 // THEN the password is either displayed in an alert or written to the page
 
+//1st click function -done
+//2nd prompt for password criteria -done
+//3rd validate password criteria for generator -done
+//4th generate password and display
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowercase = ["a", "b", "c", "d", "e", "f", "g",];
+var uppercase = ["A", "B", "C", "D"];
+var special = ["!", "@", "#", "$"];
+var number = [1, 2, 3, 4, 5];
+
+// Function for getting a random element from an array
+function getRandom(array) {
+    var randomIndex = Math.floor(Math.random() * array.length);
+    var randomElement = array[randomIndex];
+
+    return randomElement;
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -28,6 +45,73 @@ function writePassword() {
 
 function generatePassword() {
     // all your code goes here
+    var length = prompt("How long do you want your password?");
+
+    if (length < 8) {
+        alert("Your password must be more than 8 characters.");
+        return;
+    }
+
+    if (length > 128) {
+        alert("Your password must be less than 128 characters.");
+        return;
+    }
+
+    var hasLowercase = confirm("Click OK to include lowercase characters in your password");
+    var hasUppercase = confirm("Click OK to include Uppercase characters in your password");
+    var hasSpecial = confirm("Click OK to include Special characters in your password");
+    var hasNumber = confirm("Click OK to include Number characters in your password");
+
+    if (
+        hasLowercase === false &&
+        hasUppercase === false &&
+        hasSpecial === false &&
+        hasNumber === false
+    ) {
+        alert("You must select at least one character type.");
+        return;
+    }
+
+    var result = [];
+    var possibleCharacters = [];
+    var guaranteedCharacters = [];
+
+    if (hasLowercase) {
+        possibleCharacters = possibleCharacters.concat(lowercase);
+        guaranteedCharacters.push(getRandom(lowercase));
+    }
+
+    if (hasUppercase) {
+        possibleCharacters = possibleCharacters.concat(uppercase);
+        guaranteedCharacters.push(getRandom(uppercase));
+    }
+
+    if (hasSpecial) {
+        possibleCharacters = possibleCharacters.concat(special);
+        guaranteedCharacters.push(getRandom(special));
+    }
+
+    if (hasNumber) {
+        possibleCharacters = possibleCharacters.concat(number);
+        guaranteedCharacters.push(getRandom(number));
+    }
+
+    console.log(possibleCharacters);
+
+    for (var i = 0; i < length; i++) {
+        var possibleCharacter = getRandom(possibleCharacters);
+        result.push(possibleCharacter);
+    }
+
+    console.log(result);
+
+    // for (var) {
+
+
+    // }
+    //make a for loop to replace first possible character in results with guaranteed characters
+
+    //transform your result into string and [*return*] it to the user
 
 }
 
